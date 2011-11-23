5be5ca92b3db85ec9ce6a0af130a04d4fcfcc010
Cyril Gabathuler <gabathuler@gmail.com>
2011-11-23 17:34:40 +0100

    test are now working, better organized

--- src/main/java/ch/hszt/mdp/web/WelcomeController.java
+++ src/main/java/ch/hszt/mdp/web/WelcomeController.java
@@ -1,6 +1,5 @@
 package ch.hszt.mdp.web;
 
-import java.sql.Time;
 import java.util.Date;
 
 import org.springframework.stereotype.Controller;
@@ -9,10 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/welcome")
+@RequestMapping("/*")
 public class WelcomeController {
-	
-
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String welcome(Model model) {
---
+++ src/test/java/ch/hszt/mdp/service/UserServiceTest.java
@@ -0,0 +1,22 @@
+package ch.hszt.mdp.service;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:mdp-service.xml"})
+public class UserServiceTest {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Test
+	public void queryName() {
+		assertNotNull(userService.queryNick("gaba"));
+	}
+}
