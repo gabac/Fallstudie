a0e66f9efcfb79b40a939c0c25dd66b54d8b69c5
Fabian Vogler <fabian@equivalence.ch>
2011-11-25 00:56:38 +0100

    Cleanup login

--- src/main/java/ch/hszt/mdp/web/AuthController.java
+++ src/main/java/ch/hszt/mdp/web/AuthController.java
@@ -7,11 +7,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/auth")
-public class LoginController {
+public class AuthController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String login(Model model) {
 
 		return "auth/login";
+	}
+
+	@RequestMapping(value = "failed", method = RequestMethod.POST)
+	public String failed(Model model) {
+
+		return "auth/failed";
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/auth/failed.jsp
@@ -0,0 +1,11 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib prefix="t" tagdir="/WEB-INF/tags"%>
+<t:layout title="Next Social Network">
+    <jsp:body>
+      <div class="hero-unit">
+        <h1>#fail</h1>
+        <p>The login failed.</p>
+        <p><a class="btn primary large" href="/">Try again &raquo;</a></p>
+      </div>
+    </jsp:body>
+</t:layout>
