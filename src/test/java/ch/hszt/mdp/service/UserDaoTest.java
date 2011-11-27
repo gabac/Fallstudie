12b6b6eb28a3b7e573f8c15079bf544194ef4ee3
Fabian Vogler <fabian@equivalence.ch>
2011-11-27 15:29:54 +0100

    Fixed tests

--- src/test/java/ch/hszt/mdp/service/UserDaoTest.java
+++ src/test/java/ch/hszt/mdp/service/UserDaoTest.java
@@ -2,6 +2,8 @@ package ch.hszt.mdp.service;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +33,8 @@ public class UserDaoTest {
 		user.setSurname("Gabathuler");
 		user.setPassword("123");
 		user.setRepeat("123");
+		user.setBirthdate(new Date());
+		user.setCity("Baden");
 
 		userDao.save(user);
 
