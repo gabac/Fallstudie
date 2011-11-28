33b1e24eaecf31fb5a841a2943a733a5f1a32ca8
Cyril Gabathuler <gabathuler@gmail.com>
2011-11-28 18:32:38 +0100

    javadoc for userscontroller

--- src/main/java/ch/hszt/mdp/web/UsersController.java
+++ src/main/java/ch/hszt/mdp/web/UsersController.java
@@ -18,6 +18,15 @@ import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
 import ch.hszt.mdp.domain.User;
 import ch.hszt.mdp.service.UserService;
 
+/**
+ * 
+ * Controller for all request beyond "/users"
+ * The controller uses the userService to get the data from hibernate. The object is injected via Spring
+ * 
+ * @author gaba, fabian
+ *
+ */
+
 @Controller
 @RequestMapping(value = "/users")
 public class UsersController {
@@ -28,7 +37,15 @@ public class UsersController {
 	public UsersController(UserService service) {
 		this.service = service;
 	}
-
+	
+	/**
+	 * 
+	 * Used to register the binder for the photo upload
+	 * 
+	 * @param request
+	 * @param binder
+	 * @throws ServletException
+	 */
 	@InitBinder
 	protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws ServletException {
 
@@ -36,6 +53,12 @@ public class UsersController {
 		binder.registerCustomEditor(byte[].class, new ByteArrayMultipartFileEditor());
 	}
 
+	/**
+	 * Displays the register form and the User object is added to the context of the JSP.
+	 * 
+	 * @param model
+	 * @return jsp view
+	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public String getRegistrationForm(Model model) {
 
@@ -44,6 +67,13 @@ public class UsersController {
 		return "users/registration";
 	}
 
+	/**
+	 * 
+	 * Set's the session invalid and therefor the user is logged out.
+	 * 
+	 * @param session
+	 * @return returns to the homepage
+	 */
 	@RequestMapping("/logout")
 	public String logout(HttpSession session) {
 
@@ -52,6 +82,14 @@ public class UsersController {
 		return "redirect:/";
 	}
 
+	/**
+	 * 
+	 * The post request from the registration page. If there is no error the user object will be saved to the database
+	 * 
+	 * @param user
+	 * @param result
+	 * @return the jsp view
+	 */
 	@RequestMapping(method = RequestMethod.POST)
 	public String register(@Valid User user, BindingResult result) {
 
