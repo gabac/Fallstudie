b47e8f099a2f3e0f74aa81e9119603b3dec0c24e
Fabian Vogler <fabian@equivalence.ch>
2011-11-28 20:02:22 +0100

    Added AuthControllerTest

---
+++ src/test/java/ch/hszt/mdp/web/AuthControllerTest.java
@@ -0,0 +1,47 @@
+package ch.hszt.mdp.web;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:mdp-test-daos.xml" })
+public class AuthControllerTest {
+
+	private AnnotationMethodHandlerAdapter adapter;
+
+	private MockHttpServletResponse response;
+
+	private AuthController controller;
+
+	@Before
+	public void setup() {
+		adapter = new AnnotationMethodHandlerAdapter();
+		response = new MockHttpServletResponse();
+		controller = new AuthController();
+	}
+
+	@Test
+	public void testLogout() throws Exception {
+
+		MockHttpServletRequest request = new MockHttpServletRequest("POST", "/auth/logout");
+
+		MockHttpSession session = new MockHttpSession();
+		request.setSession(session);
+
+		ModelAndView mAv = adapter.handle(request, response, controller);
+
+		assertTrue(session.isInvalid());
+		assertEquals("redirect:/", mAv.getViewName());
+	}
+}
