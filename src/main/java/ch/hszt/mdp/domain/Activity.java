30c1642b6be6879f7d3d88ca42090271c16b00a0
Fabian Vogler <fabian@equivalence.ch>
2011-12-03 14:35:58 +0100

    Fixed Activity

--- src/main/java/ch/hszt/mdp/domain/Activity.java
+++ src/main/java/ch/hszt/mdp/domain/Activity.java
@@ -10,62 +10,59 @@ import javax.persistence.Transient;
 @Entity
 @Table(name = "activities")
 public class Activity {
-	
 
 	public enum ActivityType {
-		STATUS("status"), PROFILE("profile"), FRIEND("friend"), LIKE("like"), DISLIKE("dislike");  
-		  
-		 private final String value;  
-		  
-		 ActivityType(String value) {  
-		   this.value = value;  
-		 }  
-		  
-		 public static ActivityType fromValue(String value) {  
-		   if (value != null) {  
-		     for (ActivityType typ : values()) {  
-		       if (typ.value.equals(value)) {  
-		         return typ;  
-		       }  
-		     }  
-		   }  
-		  
-		   return getDefault();  
-		 }  
-		  
-		 public String toValue() {  
-		   return value;  
-		 }  
-		  
-		 public static ActivityType getDefault() {  
-		   return STATUS;  
-		 }  
+		STATUS("status"), PROFILE("profile"), FRIEND("friend"), LIKE("like"), DISLIKE("dislike");
+
+		private final String value;
+
+		ActivityType(String value) {
+			this.value = value;
+		}
+
+		public static ActivityType fromValue(String value) {
+			if (value != null) {
+				for (ActivityType typ : values()) {
+					if (typ.value.equals(value)) {
+						return typ;
+					}
+				}
+			}
+
+			return getDefault();
+		}
+
+		public String toValue() {
+			return value;
+		}
+
+		public static ActivityType getDefault() {
+			return STATUS;
+		}
 	}
-	
+
 	@Id
 	@GeneratedValue
 	private Integer activity_id;
-	
+
 	private Integer user_id;
-	
-	@Column(name="typ")
+
+	@Column(name = "typ")
 	private String typValue;
-	
-	@Transient  
-	 public ActivityType getActivityType() {  
-	  return ActivityType.fromValue(typValue);  
-	 }  
-	  
-	 public void setTyp(ActivityType typ) {  
-	  this.typValue = typ.toValue();  
-	 }  
-	
-	private String text;
-	
+
+	@Transient
+	public ActivityType getActivityType() {
+		return ActivityType.fromValue(typValue);
+	}
+
+	public void setTyp(ActivityType typ) {
+		this.typValue = typ.toValue();
+	}
+
 	private String content;
-	
+
 	public Activity() {
-		
+
 	}
 
 	public Integer getActivity_id() {
@@ -84,14 +81,6 @@ public class Activity {
 		this.user_id = user_id;
 	}
 
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
 	public String getContent() {
 		return content;
 	}
@@ -99,5 +88,4 @@ public class Activity {
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
 }
