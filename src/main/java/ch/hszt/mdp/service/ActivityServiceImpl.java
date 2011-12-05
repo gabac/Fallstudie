47b96bf7afeede2e22cfb286f5e2655b8e74a3d8
Cyril Gabathuler <gabathuler@gmail.com>
2011-12-05 23:22:58 +0100

    Better activity message

--- src/main/java/ch/hszt/mdp/service/ActivityServiceImpl.java
+++ src/main/java/ch/hszt/mdp/service/ActivityServiceImpl.java
@@ -13,50 +13,49 @@ import ch.hszt.mdp.domain.Activity.ActivityType;
 
 @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 public class ActivityServiceImpl implements ActivityService {
-	
+
 	private ActivityDao activityDao;
-	
+
 	public void setActivityDao(ActivityDao activityDao) {
 		this.activityDao = activityDao;
 	}
-	
+
 	public List<Activity> getActivities() {
 		return activityDao.getActivities();
 	}
-	
+
 	public void create(Activity activity) {
 		activityDao.save(activity);
 	}
-	
+
 	/**
-	 * Accept Friends by clicking on the accept button
-	 * following are executed:
-	 * create a new Activity Object by filling out the attributes for the user and the friend
-	 * make an insert into with activity.save(activity)
+	 * Accept Friends by clicking on the accept button following are executed: create a new Activity Object by filling
+	 * out the attributes for the user and the friend make an insert into with activity.save(activity)
 	 * 
 	 * @author Roger Bollmann
 	 * 
 	 */
 	public void acceptFriendship(User friend, User user) {
-		
-		//make a status for the user: You are now friend with "friend"
+
+		// make a status for the user: You are now friends with "friend"
 		Activity activity = new Activity();
 		activity.setContent("You are now friends with " + friend.getPrename() + " " + friend.getSurname());
 		activity.setTyp(ActivityType.FRIEND);
 		activity.setUser(user);
 		activity.setTime(new DateTime());
-		
-		//make a status for the friend: You are now friend with "user"
+
+		// make a status for the friend: "friend" is now friends with "user"
 		Activity activity2 = new Activity();
-		activity2.setContent("You are now friends with " + user.getPrename() + " " + user.getSurname());
+		activity2.setContent(friend.getPrename() + " " + friend.getSurname() + " is now friends with " + user.getPrename() + " "
+				+ user.getSurname());
 		activity2.setTyp(ActivityType.FRIEND);
 		activity2.setUser(friend);
 		activity2.setTime(new DateTime());
-		
+
 		activityDao.save(activity);
-		
+
 		activityDao.save(activity2);
-		
+
 	}
-	
+
 }
