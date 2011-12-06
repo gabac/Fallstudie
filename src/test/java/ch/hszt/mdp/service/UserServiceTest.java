2cb96ef51eff0a0d63d7006ead4d3708e33a18ee
Raphael Marques <raphi.rm@gmail.com>
2011-12-06 10:58:49 +0100

    Added tests for Update Class

--- src/test/java/ch/hszt/mdp/service/UserServiceTest.java
+++ src/test/java/ch/hszt/mdp/service/UserServiceTest.java
@@ -1,7 +1,7 @@
 package ch.hszt.mdp.service;
 
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertTrue;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
@@ -12,6 +12,8 @@ import org.junit.runner.RunWith;
 
 import ch.hszt.mdp.dao.UserDao;
 import ch.hszt.mdp.domain.User;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @RunWith(JMock.class)
 public class UserServiceTest {
@@ -56,29 +58,65 @@ public class UserServiceTest {
 
 		return user;
 	}
-//        @Test
-//        public void testPasswordUpdate(){
-//            final UserDao dao = context.mock(UserDao.class);
-//            final UserDao dao2 = context.mock(UserDao.class);
-//            final User user = getUser();
-//            user.setPassword("changeit");
-//            user.setRepeat("changeit");
-//            final User origin = getUser();
-//            origin.setId(1);
-//            context.checking(new Expectations() {
-//			{
-//				one(dao).save(user);
-//                                one(dao2).save(origin);
-//			}
-//		});
-//            service.setUserDao(dao);
-//            service.create(origin);
-//            service.setUserDao(dao2);
-//            User last =  service.updateUser(origin, user);
-//            assertEquals(last, origin);
-//            
-//        }
-	
+        @Test
+        public void testPasswordNotUpdated(){
+            final UserDao dao = context.mock(UserDao.class);
+            final User user = getUser();
+            user.setPassword("changeit");
+            user.setRepeat("changeit");
+            User u2 = getUser();
+            u2.setPassword("changeit");
+            u2.setRepeat("changeit");
+            final User origin = getUser();
+            origin.setId(1);
+            context.checking(new Expectations() {
+			{
+				one(dao).save(origin);
+			}
+		});
+            service.setUserDao(dao);
+            service.create(origin);
+            context.checking(new Expectations() {
+			{
+				one(dao).save(origin);
+			}
+		});
+            User last =  service.updateUser(origin, user);
+            assertTrue(!last.getPassword().equals(u2.getPassword()));
+            
+        }
+	@Test
+        public void testPasswordUpdated(){
+            final UserDao dao = context.mock(UserDao.class);
+            final User user = getUser();
+            user.setPassword("456");
+            user.setRepeat("456");
+            final User u2 = getUser();
+            u2.setPassword("456");
+            try{
+                u2.setPassword(sha1(u2.getPassword()));
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+            
+            final User origin = getUser();
+            origin.setId(1);
+            context.checking(new Expectations() {
+			{
+				one(dao).save(origin);
+			}
+		});
+            service.setUserDao(dao);
+            service.create(origin);
+            context.checking(new Expectations() {
+			{
+				one(dao).save(origin);
+			}
+		});
+            User last =  service.updateUser(origin, user);
+            assertTrue(last.getPassword().equals(u2.getPassword()));
+            
+        }
 	private void getAcceptedFriends(){
 		UserServiceImpl userService = new UserServiceImpl();
 		
@@ -91,6 +129,22 @@ public class UserServiceTest {
 		
 		
 	}
-	
+	 public String sha1(String password) throws NoSuchAlgorithmException {
+
+		MessageDigest md = MessageDigest.getInstance("SHA1");
+		md.reset();
+
+		byte[] buffer = password.getBytes();
+		md.update(buffer);
+
+		byte[] digest = md.digest();
+
+		String hexStr = "";
+		for (int i = 0; i < digest.length; i++) {
+			hexStr += Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1);
+		}
+
+		return hexStr;
+	}
 	
 }
