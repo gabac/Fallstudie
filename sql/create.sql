4ca3a28febe1873337af3b0c742a13efe2d0fd09
Fabian Vogler <fabian@equivalence.ch>
2011-12-15 18:53:06 +0100

    Revert "Changed DB-design"
    This reverts commit 472df4056e03503ac473a5d75333270abf37c9cd.
    
    Conflicts:
    
    	sql/create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,8 +1,6 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS status;
 DROP TABLE IF EXISTS friendship;
-DROP TABLE IF EXISTS friendship_accepted;
-DROP TABLE IF EXISTS friendship_requested;
 DROP VIEW IF EXISTS friends;
 DROP VIEW IF EXISTS roles;
 DROP TABLE IF EXISTS activities;
@@ -17,24 +15,19 @@ CREATE TABLE activities  (
 )
 ENGINE = InnoDB
 CHARACTER SET utf8
-COLLATE utf8_general_ci;
+COLLATE utf8_general_ci
+AUTO_INCREMENT = 8;
 
-CREATE TABLE `friendship_accepted` (
-	`primary_user` INT(11) NOT NULL,
-	`secondary_user` INT(11) NOT NULL,
-	PRIMARY KEY (`primary_user`, `secondary_user`)
+CREATE TABLE friendship  ( 
+	primary_user  	int(11) NOT NULL,
+	secondary_user	int(11) NOT NULL,
+	accepted      	tinyint(1) NOT NULL DEFAULT '0',
+	PRIMARY KEY(primary_user,secondary_user)
 )
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB;
-
-CREATE TABLE `friendship_requested` (
-	`primary_user` INT(11) NOT NULL,
-	`secondary_user` INT(11) NOT NULL,
-	PRIMARY KEY (`primary_user`, `secondary_user`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB;
-
+ENGINE = InnoDB
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+AUTO_INCREMENT = 0;
 
 CREATE TABLE status  ( 
 	status_id  	int(11) AUTO_INCREMENT NOT NULL,
@@ -44,7 +37,8 @@ CREATE TABLE status  (
 )
 ENGINE = InnoDB
 CHARACTER SET utf8
-COLLATE utf8_general_ci;
+COLLATE utf8_general_ci
+AUTO_INCREMENT = 1;
 
 CREATE TABLE users  ( 
 	id       	int(11) UNSIGNED AUTO_INCREMENT NOT NULL,
@@ -59,16 +53,12 @@ CREATE TABLE users  (
 )
 ENGINE = InnoDB
 CHARACTER SET utf8
-COLLATE utf8_general_ci;
+COLLATE utf8_general_ci
+AUTO_INCREMENT = 5;
 
-CREATE VIEW friends_accepted
+CREATE VIEW friends
 AS
-select `u1`.`surname` AS `user_surname`,`u1`.`prename` AS `user_prename`,`u2`.`surname` AS `friends_surname`,`u2`.`prename` AS `friends_prename` from `socialnetwork`.`friendship_accepted` `fs` join `socialnetwork`.`users` `u1` join `socialnetwork`.`users` `u2` where ((`u1`.`id` = `fs`.`primary_user`) and (`u2`.`id` = `fs`.`secondary_user`));
-
-CREATE VIEW friends_requested
-AS
-select `u1`.`surname` AS `user_surname`,`u1`.`prename` AS `user_prename`,`u2`.`surname` AS `friends_surname`,`u2`.`prename` AS `friends_prename` from `socialnetwork`.`friendship_requested` `fs` join `socialnetwork`.`users` `u1` join `socialnetwork`.`users` `u2` where ((`u1`.`id` = `fs`.`primary_user`) and (`u2`.`id` = `fs`.`secondary_user`));
-
+select `u1`.`surname` AS `user_surname`,`u1`.`prename` AS `user_prename`,`u2`.`surname` AS `friends_surname`,`u2`.`prename` AS `friends_prename` from `socialnetwork`.`friendship` `fs` join `socialnetwork`.`users` `u1` join `socialnetwork`.`users` `u2` where ((`u1`.`id` = `fs`.`primary_user`) and (`u2`.`id` = `fs`.`secondary_user`) and (`fs`.`accepted` = 1));
 
 CREATE VIEW roles
 AS
@@ -89,30 +79,29 @@ INSERT INTO activities(activity_id, user_id, typ, parent, time, content)
   VALUES(6, 2, 'status', NULL, '2011-11-20 12:21:33.0', 'Ich bin so doof, ich kÃ¶nnte eine Kuh essen');
 INSERT INTO activities(activity_id, user_id, typ, parent, time, content)
   VALUES(7, 2, 'status', 2, '2011-11-23 13:22:22.0', 'Ich kommentiere gerne Kommentare');
-INSERT INTO friendship_requested(primary_user, secondary_user)
-  VALUES(1, 2);
-INSERT INTO friendship_accepted(primary_user, secondary_user)
-  VALUES(1, 3);
-INSERT INTO friendship_accepted(primary_user, secondary_user)
-  VALUES(1, 4);
-INSERT INTO friendship_requested(primary_user, secondary_user)
-  VALUES(2, 3);
-INSERT INTO friendship_accepted(primary_user, secondary_user)
-  VALUES(2, 4);
-INSERT INTO friendship_accepeted(primary_user, secondary_user)
-  VALUES(3, 4);
+INSERT INTO friendship(primary_user, secondary_user, accepted)
+  VALUES(1, 2, 0);
+INSERT INTO friendship(primary_user, secondary_user, accepted)
+  VALUES(1, 3, 1);
+INSERT INTO friendship(primary_user, secondary_user, accepted)
+  VALUES(1, 4, 1);
+INSERT INTO friendship(primary_user, secondary_user, accepted)
+  VALUES(2, 3, 0);
+INSERT INTO friendship(primary_user, secondary_user, accepted)
+  VALUES(2, 4, 1);
+INSERT INTO friendship(primary_user, secondary_user, accepted)
+  VALUES(3, 4, 1);
   
 INSERT INTO users(id, email, password, prename, surname, birthdate, city, photo)
-  VALUES(1, 'raphi.rm@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Raphael', 'Marques', '1988-09-16', 'Zürich', '');
+  VALUES(1, 'raphi.rm@gmail.com', '2ecb44da5fdeda0a2307f1c6ae8dcf4df476e588', 'Raphael', 'Marques', '1988-09-16', 'Zrich', '');
 INSERT INTO users(id, email, password, prename, surname, birthdate, city, photo)
-  VALUES(2, 'roger_bollmann#@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Roger', 'Bollmann', '1922-12-31', 'Zürich', '');
+  VALUES(2, 'roger_bollmann#@gmail.com', 'a61a1add71dadc74037ae5e44ee30713db9a7451', 'Roger', 'Bollmann', '1922-12-31', 'Zrich', '');
 INSERT INTO users(id, email, password, prename, surname, birthdate, city, photo)
-  VALUES(3, 'c_i*ril1988@bluewineriani.do', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Cyril', 'G2Ba', '1711-09-30', 'Baden baden bden', '');
+  VALUES(3, 'c_i*ril1988@bluewineriani.do', '46c0ec0a40c76d7f8a7ba9519171bcc2e60efa51', 'C_i)ril', 'G2Ba', '1711-09-30', 'Baden baden bden', '');
 INSERT INTO users(id, email, password, prename, surname, birthdate, city, photo)
   VALUES(4, 'fabian.vogler@bluewin.ch', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'Fabian', 'Vogler', '1989-01-01', 'Baden', '');
- 
- 
-  ALTER TABLE activities
+
+ALTER TABLE activities
 	ADD CONSTRAINT activities_ibfk_1
 	FOREIGN KEY(parent)
 	REFERENCES activities(activity_id)
