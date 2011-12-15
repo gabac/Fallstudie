764d33e9164b287745d17764d0a7b5232ef583dd
Fabian Vogler <fabian@equivalence.ch>
2011-12-15 08:36:30 +0100

    Added missing test method

--- src/test/java/ch/hszt/mdp/dao/FriendshipDaoTest.java
+++ src/test/java/ch/hszt/mdp/dao/FriendshipDaoTest.java
@@ -1,6 +1,7 @@
 package ch.hszt.mdp.dao;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -8,20 +9,19 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import ch.hszt.mdp.domain.Friendship;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:mdp-test-daos.xml" })
 public class FriendshipDaoTest {
-	
+
 	private FriendshipDao friendshipDao;
-	
+
 	private Friendship friendship;
-	
+
 	@Autowired
-	public void setFriendshipDao(FriendshipDao friendshipDao){
+	public void setFriendshipDao(FriendshipDao friendshipDao) {
 		this.friendshipDao = friendshipDao;
 	}
-	
+
 	@Before
 	public void setup() {
 		friendship = new Friendship();
@@ -29,6 +29,9 @@ public class FriendshipDaoTest {
 		friendship.setSecondary_user(2);
 		friendship.setAccepted(1);
 	}
-	
 
+	@Test
+	public void testFriendship() {
+
+	}
 }
